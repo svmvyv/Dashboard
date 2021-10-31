d78f9a9a27597be8f84726a61374d9d8048d3923
Fatimah Alhelal <31361308+fatmahhelal@users.noreply.github.com>
2021-10-31 13:02:17 +0300

    v1

---
+++ js/charts.js
@@ -0,0 +1,12 @@
+
+// Line Chart for Orders
+var ordersChart = document.getElementById("myAreaChart");
+
+// Bar Chart for Visitor
+var visitorChart = document.getElementById("myBarChart");
+
+// Liner Chart for Cities
+var citiesChart = document.getElementById("myPieChart");
+
+// Liner Chart for Rat
+var ratChart = document.getElementById("myLinearChart");
