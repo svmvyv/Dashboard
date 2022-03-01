4ea9552185b3c55ed3e6a9b02ed364007de4a930
svmvyv <samayanader@gmail.com>
2022-03-01 14:49:34 +0300

    finalizing edits

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,12 @@
 var menu_btn = document.querySelector("#menu-btn");
 var sidebar = document.querySelector("#sidebar");
 var container = document.querySelector(".my-container");
+var footer = document.querySelector("#ft");
 menu_btn.addEventListener("click", () => {
     sidebar.classList.toggle("active-nav");
     container.classList.toggle("active-cont");
+    footer.classList.toggle("active-ft");
+
 });
 
 
